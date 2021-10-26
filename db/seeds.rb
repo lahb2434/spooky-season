a095fc8100109a05ba1beb20fc062176d3d5fb98
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-10-25 21:03:34 -0400

    notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bat = "http://assets.stickpng.com/images/580b57fbd9996e24bc43bb3e.png"
+blood_splatter = "https://www.pngkit.com/png/detail/20-200208_blood-splatter-by-crazehpivotkid-on-clipart-library-transparent.png"
+michael_myers = "https://www.sideshow.com/storage/product-images/100398/michael-myers-deluxe_halloween_silo.png"
+pumpkin = "http://assets.stickpng.com/images/580b57fcd9996e24bc43c3db.png"
+chucky = "https://static.wikia.nocookie.net/vsdebating/images/0/0d/ED2419B3-90F5-4F40-8A90-107FE3A8A5C1.png/revision/latest/scale-to-width-down/392?cb=20190803151701"
+freddy_krueger = "https://www.sideshow.com/storage/product-images/100359/freddy-krueger_nightmare-on-elm-street_silo.png"
+jason_vhoores = "https://www.sideshow.com/storage/product-images/100360/jason-voorhees_friday-the-13th_silo.png"
+leatherface = "https://www.sideshow.com/storage/product-images/100399/leatherface_texas-chainsaw-massacre_silo.png"
+ghost_girl = "https://snipstock.com/assets/cdn/png/2943a4bab439a3542aefc8d1cc6c551b.png"
+full_moon = "https://snipstock.com/assets/cdn/png/ca380a688d046ae22386834857e034bd.png"
+black_spider = "http://assets.stickpng.com/images/580b57fbd9996e24bc43bc73.png"

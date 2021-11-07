58f85608e7450cad5a77274e3c1f0ec54114e81c
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-06 21:47:01 -0400

    save new cards objects to db

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,28 +3,37 @@ class CardsController < ApplicationController
   def index
     cards = Card.all
 
-    render json: cards.as_json( :only => [:name],
-      :include => { 
-        :element_positions => { 
-          :include => { 
-            :element => { :only => [:name, :image_url] }
-          },
-        :only => [:x_position, :y_position]
+    render json: cards.as_json( only: [:name],
+      include: { 
+        element_positions: { 
+          only: [:id, :xPosition, :yPosition, :xOffset, :yOffset, :element_id, :imageSize, :name ]
+        },
+        backgrounds: {
+          only: [:id, :name]
         }
-      } 
+      }
     )
   end
 
   def create
-    byebug
-    # mark = Element.all.detect{|x| x.name == card_params[:card_elements][0][:name].gsub(/[_\d]/, '')}
-    card = Card.new(card_params)
+   
+    card = Card.new(name: card_params[:name])
+    background = Background.find(card_params[:background])
+    card.backgrounds = [background]
+    card_params[:card_elements].each do |element_attributes|
+      element_position = card.element_positions.new(element_attributes)
+      element = Element.all.detect{|x| x.name == element_attributes[:name][0..-3]}
+      element_position.element = element
+    end
+  
     if card.save
       render json: card, status: :accepted
     else
       render json: {errors: card.errors.full_messages}, status: :unprocessible_entity
     end
   end
+
+  
 
   private
 
